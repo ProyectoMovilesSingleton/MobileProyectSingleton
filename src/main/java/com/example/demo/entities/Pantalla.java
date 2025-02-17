2ed835c5b6058a4b8b823bcd0749ba2dc413a4b1
BartolomeGarcia <bgarciar11@iesarroyoharnina.es>
2025-02-17 18:39:41 +0100

    Create Pantalla.java
    Entity Pantalla

---
+++ src/main/java/com/example/demo/entities/Pantalla.java
@@ -0,0 +1,30 @@
+package com.example.demo.entities;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Pantalla {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NonNull
+	private double tamanno;
+	@NonNull
+	private String tecnologia;
+	
+	public Pantalla(double tamanno, String tecnologia) {
+		super();
+		this.tamanno = tamanno;
+		this.tecnologia = tecnologia;
+	}
+}
