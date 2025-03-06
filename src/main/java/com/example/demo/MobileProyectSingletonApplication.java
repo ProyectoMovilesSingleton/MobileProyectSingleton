d3d759ce831e542747c0a2f828ec6abee945dd50
PMgalann <156293179+PMgalann@users.noreply.github.com>
2025-03-06 20:41:21 +0100

    Soluccion CORS | Para que funcione con Angular

--- src/main/java/com/example/demo/MobileProyectSingletonApplication.java
+++ src/main/java/com/example/demo/MobileProyectSingletonApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MobileProyectSingletonApplication {
@@ -9,5 +12,18 @@ public class MobileProyectSingletonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MobileProyectSingletonApplication.class, args);
 	}
-
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS en todos los endpoints
+                        .allowedOrigins("http://localhost:4200") // Origen permitido (Angular)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                        .allowedHeaders("*") // Headers permitidos
+                        .allowCredentials(true); // Permitir credenciales (cookies, headers de autenticación, etc.)
+            }
+        };
+    }
 }
